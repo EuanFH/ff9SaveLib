e31893f09d287d48ec9ac59ef2de4739fd0f583a
Euan Haahr <euanhaahrmail2@gmail.com>
2020-10-15 21:34:36 +0100

    added completed marshal json files implementation

--- savedData.go
+++ savedData.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"github.com/euanfh/ff9SaveLib/Crypto"
 	"io/ioutil"
+	"math"
+	"os"
 	"regexp"
 	"strconv"
 )
@@ -21,7 +23,6 @@ type SavedData struct {
 	Slot [150]File
 }
 
-//if i put into the unmarshal function to set these fields up properly i wouldnt need this
 func NewSavedData() SavedData {
 	return SavedData{
 		MetaData: NewMetaData(),
@@ -29,26 +30,95 @@ func NewSavedData() SavedData {
 }
 
 func(sd *SavedData) MarshalJsonFiles(directory string) error {
+	if _, err := os.Stat(directory); os.IsNotExist(err) {
+    	if err := os.Mkdir(directory, 0755); err != nil {
+    		return err
+		}
+	}
+	//File Info
+	fileInfoBytes, err := json.Marshal(&sd.MetaData.FileInfo)
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(directory + "/SLOTINFO", fileInfoBytes, 0644); err != nil {
+		return err
+	}
+
+	var prefsLanguage PrefsLanguage
+	languageString, ok := LanguageIntToLanguageString()[sd.MetaData.SelectedLanguage]
+	if !ok {
+		return err
+	}
+	prefsLanguage.Value = languageString
+	prefsLanguageBytes, err := json.Marshal(prefsLanguage)
+	if err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(directory +"/PREFS_Language", prefsLanguageBytes, 0644); err != nil {
+		return err
+	}
+
+	//file previews
+	for filePreviewPos, filePreview := range sd.FilePreviews {
+		if filePreview == (FilePreview{}) {
+			continue
+		}
+		fileName := generateSaveFileName("PREVIEW", filePreviewPos)
+		filePreviewBytes, err := json.Marshal(&filePreview)
+		if err != nil {
+			return err
+		}
+		if err := ioutil.WriteFile(directory + "/" + fileName, filePreviewBytes, 0644); err != nil {
+			return err
+		}
+	}
+
+	//auto save
+	fileBytes, err := json.Marshal(&sd.Auto)
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(directory + "/AUTO", fileBytes, 0644); err != nil {
+		return err
+	}
+
+	//save files
+	for filePos, file := range sd.Slot {
+		if file == (File{}) {
+			continue
+		}
+		fileName := generateSaveFileName("DATA", filePos)
+		fileBytes, err := json.Marshal(&file)
+		if err != nil {
+			return err
+		}
+		if err := ioutil.WriteFile(directory + "/" + fileName, fileBytes, 0644); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
-//could possibly make marshal json files into an interface not sure
 func(sd *SavedData) UnmarshalJsonFiles(directory string) error {
+	//metadata
 	fileInfoBytes, err := ioutil.ReadFile(directory +"/SLOTINFO")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	if err := json.Unmarshal(fileInfoBytes, &sd.MetaData.FileInfo); err != nil {
-		panic(err)
+		return err
 	}
 
 	var prefsLanguage PrefsLanguage
 	prefsLanguageBytes, err := ioutil.ReadFile(directory +"/PREFS_Language")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	if err := json.Unmarshal(prefsLanguageBytes, &prefsLanguage); err != nil {
-		panic(err)
+		return err
 	}
 	languageInt, ok := LanguageStringToLanguageInt()[prefsLanguage.Value]
 	if !ok {
@@ -56,49 +126,54 @@ func(sd *SavedData) UnmarshalJsonFiles(directory string) error {
 	}
 	sd.MetaData.SelectedLanguage = languageInt
 
+	//file previews
 	files, err := ioutil.ReadDir(directory)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	for _, file := range files {
-		fileNo := getFileNumber("PREVIEW", file.Name())
-		if fileNo == -1 {
+		fileNo := getFilePos("PREVIEW", file.Name())
+		if fileNo == -1 || fileNo > len(sd.Slot) {
 			continue
 		}
 		previewBytes, err := ioutil.ReadFile(directory + "/" + file.Name())
 		if err != nil {
-			panic(err)
+			return err
 		}
 		if err := json.Unmarshal(previewBytes, &sd.FilePreviews[fileNo]); err != nil {
-			panic(err)
+			return err
 		}
 
 	}
+
+	//auto save
 	fileBytes, err := ioutil.ReadFile(directory + "/AUTO")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	if err := json.Unmarshal(fileBytes, &sd.Auto); err != nil {
-		panic(err)
+		return err
 	}
+
+	//save files
 	for _, file := range files {
-		fileNo := getFileNumber("DATA", file.Name())
+		fileNo := getFilePos("DATA", file.Name())
 		if fileNo == -1 {
 			continue
 		}
 		fileBytes, err := ioutil.ReadFile(directory + "/" + file.Name())
 		if err != nil {
-			panic(err)
+			return err
 		}
 		if err := json.Unmarshal(fileBytes, &sd.Slot[fileNo]); err != nil {
-			panic(err)
+			return err
 		}
 
 	}
 	return nil
 }
 
-func getFileNumber(prefix string, fileName string) int{
+func getFilePos(prefix string, fileName string) int{
 	re := regexp.MustCompile(fmt.Sprintf(`%s_SLOT(?P<SLOT>\d{1,2})_FILE(?P<FILE>\d{1,2})$`, prefix))
 	slotFile := re.FindStringSubmatch(fileName)
 	if slotFile == nil {
@@ -117,6 +192,17 @@ func getFileNumber(prefix string, fileName string) int{
 		return -1
 	}
 	return slotNo * FilesPerSlot + fileNo
+}
+
+func getSlotAndFileNumber(filePos int) (int, int){
+	slotNo := int(math.Floor(float64(filePos / FilesPerSlot)))
+	fileNo := filePos - slotNo * FilesPerSlot
+	return slotNo, fileNo
+}
+
+func generateSaveFileName(prefix string, filePos int) string{
+	slotNo, fileNo := getSlotAndFileNumber(filePos)
+	return fmt.Sprintf("%s_SLOT%d_FILE%d", prefix, slotNo, fileNo)
 }
 
 func(sd *SavedData) BinaryUnmarshaler(data []byte) error{
@@ -200,5 +286,3 @@ func(sd *SavedData) BinaryMarshaler() ([]byte, error){
 	}
 	return buf.Bytes(), nil
 }
-
-
